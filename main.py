83ad1be3e48ec33d2102a1959d1e13eda4a9509d
shamil <samax.zer@gmail.com>
2024-09-23 00:36:43 +0300

    added email check and removed comments

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ emails = Table('emails', meta, Column('id', Integer, primary_key=True), Column('
 
 bot = telebot.TeleBot(f'{bot_token}')
 
-new_message = ''
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -22,15 +20,15 @@ def send_welcome(message):
                      "Этот бот используется для проверок телефона и email. Напиши /check_phone или /check_email чтобы начать")
 
 
+@bot.message_handler(commands=['check_phone'])
+def on_check_phone(message):
+    bot.send_message(message.from_user.id, "Введи номер телефона")
+    bot.register_next_step_handler(message, on_enter_phone)
+
+
 @bot.message_handler(commands=['enter_phone'])
 def on_enter_phone(message):
-    # bot.send_message(message.from_user.id, "Введи номер телефона")
-    # if not message.text.strip():
-    #     bot.send_message(message.from_user.id, "Поле не может быть пустым")
-    # else:
-    #     bot.register_next_step_handler(message, on_check_phone)
-    selected_phone = phones.select().where(phones.c.phone == message.text)
-    # selected_phone = phones.select().where(phones.c.phone == new_message)
+    selected_phone = phones.select().where(phones.c.phone == message.text.strip())
     connection = engine.connect()
     result = connection.execute(selected_phone)
 
@@ -42,64 +40,30 @@ def on_enter_phone(message):
         bot.send_message(message.from_user.id, 'Данный номер отсутствует в БД ☹️')
 
 
-@bot.message_handler(commands=['check_phone'])
+@bot.message_handler(commands=['check_email'])
 def on_check_phone(message):
-    bot.send_message(message.from_user.id, "Введи номер телефона")
-    bot.register_next_step_handler(message, on_enter_phone)
-    # if not message.text.strip():
-    # 	bot.send_message(message.from_user.id,
-    # 					 "Поле не может быть пустым")
-    #  message.text.strip():
-    # global new_message
-    # new_message = message.text
-    # selected_phone = phones.select().where(phones.c.phone == message.text)
-    # # selected_phone = phones.select().where(phones.c.phone == new_message)
-    # connection = engine.connect()
-    # result = connection.execute(selected_phone)
-    #
-    # if result.first() is not None:
-    #     print('phone has been found')
-    #     bot.send_message(message.from_user.id, 'Данный номер присутствует в БД')
-    # else:
-    #     print('phone is absent')
-    #     bot.send_message(message.from_user.id, 'Данный номер отсутствует в БД')
+    bot.send_message(message.from_user.id, "Введи email")
+    bot.register_next_step_handler(message, on_enter_email)
 
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-# 	bot.send_message(message.from_user.id, 'Данная команда недоступна. Используй /check_phone или /check_email')
+@bot.message_handler(commands=['enter_email'])
+def on_enter_email(message):
+    selected_email = emails.select().where(emails.c.email == message.text.strip())
+    connection = engine.connect()
+    result = connection.execute(selected_email)
+
+    if result.first() is not None:
+        print('email has been found')
+        bot.send_message(message.from_user.id, 'Данный email присутствует в БД 😀')
+    else:
+        print('email is absent')
+        bot.send_message(message.from_user.id, 'Данный email отсутствует в БД ☹️')
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if (message.text != '/check_phone') or (message.text != '/check_email'):
         bot.send_message(message.from_user.id, "Данная команда недоступна. Используй /check_phone или /check_email")
-    elif message.text == '/check_phone':
-        # bot.send_message(message.from_user.id, "Введи номер телефона")
-        # new_message = message.text
-        bot.register_next_step_handler(message, on_enter_phone)
-
-
-# elif message.text == '/check_email':
-# bot.register_next_step_handler(message, on_email_check)
-# elif not message.text.strip():
-# 	bot.send_message(message.from_user.id, "Поле не может быть пустым")
-# elif message.text is '/check_phone':
-# selected_phone = phones.select().where(phones.c.phone == message.text)
-# connection = engine.connect()
-# result = connection.execute(selected_phone)
-#
-# if result.first() is not None:
-# 	print('nayden')
-# 	bot.reply_to(message, 'Данный номер присутствует в БД')
-# else:
-# 	print('absent')
-# 	bot.reply_to(message, 'Данный номер отсутствует в БД')
-
-# elif message.text == "/check_phone":
-# 	bot.send_message(message.from_user.id, "Напиши номер телефона, который хочешь проверить")
-# else:
-# 	bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-# bot.send_message(message.from_user.id, 'Данная команда недоступна. Используй /check_phone или /check_email')
 
 
 bot.infinity_polling()
