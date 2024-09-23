3d37f6284ebd8ff3f6ada71c38ef0dd7eae158be
shamil <samax.zer@gmail.com>
2024-09-23 15:14:06 +0300

    added password check

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ bot = telebot.TeleBot(f'{bot_token}')
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.from_user.id,
-                     "Этот бот используется для проверок телефона и email. Напиши /check_phone или /check_email чтобы начать")
+                     f"Этот бот используется для проверок телефона и email. Используй {'/check_phone'}, {'/ check_email'} или {'/check_password'}, чтобы начать")
 
 
 @bot.message_handler(commands=['check_phone'])
@@ -60,10 +60,54 @@ def on_enter_email(message):
         bot.send_message(message.from_user.id, 'Данный email отсутствует в БД ☹️')
 
 
+@bot.message_handler(commands=['check_password'])
+def on_check_password(message):
+    bot.send_message(message.from_user.id, "Введи пароль")
+    bot.register_next_step_handler(message, on_enter_password)
+
+
+@bot.message_handler(commands=['enter_password'])
+def on_enter_password(message):
+    digits = '1234567890'
+    upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower_letters = 'abcdefghijklmnopqrstuvwxyz'
+    symbols = '!@#$%^&*()-+'
+    acceptable = digits + upper_letters + lower_letters + symbols
+    password = message.text.strip()
+    passwd = set(password)
+
+    if any(char not in acceptable for char in passwd):
+        bot.send_message(message.from_user.id, 'Уупс, был введён запрещённый символ - введи другой пароль')
+        bot.register_next_step_handler(message, on_enter_password)
+    else:
+        recommendations = []
+        if len(password) < 8:
+            recommendations.append(f'увеличить число символов - {12 - len(password)}')
+            bot.send_message(message.from_user.id, 'Слишком короткий пароль, попробуй ввести другой')
+            bot.register_next_step_handler(message, on_enter_password)
+            return
+
+        for what, errMessage in ((digits, 'цифру'),
+                                 (symbols, 'спецсимвол'),
+                                 (upper_letters, 'заглавную букву'),
+                                 (lower_letters, 'строчную букву')):
+            if all(char not in what for char in passwd):
+                recommendations.append(f'Добавь 1 {errMessage}')
+
+        if recommendations:
+            bot.send_message(message.from_user.id, f"Слабый пароль. Рекомендации:")
+            for recommendation in recommendations:
+                bot.send_message(message.from_user.id, f"{recommendation}")
+
+        else:
+            bot.send_message(message.from_user.id, 'Хороший пароль, nice job')
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if (message.text != '/check_phone') or (message.text != '/check_email'):
-        bot.send_message(message.from_user.id, "Данная команда недоступна. Используй /check_phone или /check_email")
+        bot.send_message(message.from_user.id,
+                         f"Данная команда недоступна. Используй {'/check_phone'}, {'/check_email'} или {'/check_password'}")
 
 
 bot.infinity_polling()
